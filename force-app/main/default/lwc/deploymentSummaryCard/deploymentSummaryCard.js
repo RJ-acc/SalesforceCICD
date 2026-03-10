3ae326c14be94f634bb7d687e3d69650c23c747d
Ravi <ravi.e.ravi@accenture.com>
2026-03-10 17:13:29 +0530

    checking delta when feature merged to develop branch

--- force-app/main/default/lwc/deploymentSummaryCard/deploymentSummaryCard.js
+++ force-app/main/default/lwc/deploymentSummaryCard/deploymentSummaryCard.js
@@ -1,6 +1,8 @@
 import { api, LightningElement } from 'lwc';
 
-const ZERO = 0;
+const ZERO = 0,
+    READY_STATUS = 'Status: Ready for Deployment',
+    ACTION_REQUIRED_STATUS = 'Status: Action Required';
 
 export default class DeploymentSummaryCard extends LightningElement {
     @api cardTitle = 'Deployment Summary';
@@ -10,8 +12,8 @@ export default class DeploymentSummaryCard extends LightningElement {
 
     get statusText() {
         if (this.failedChecks > ZERO) {
-            return 'Status: Action Required';
+            return ACTION_REQUIRED_STATUS;
         }
-        return 'Status: Ready for Deployment';
+        return READY_STATUS;
     }
 }
